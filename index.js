c58878b45685de4448d01296fa73caad503777dd
prasannak <119028680+prasannasalt@users.noreply.github.com>
2024-06-21 12:25:25 +0530

    wish commit

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const path = require('path');
 
+const app = express();
+const port = 3000;
+
+// Serve static files (index.html, images, CSS, etc.)
+app.use(express.static(path.join(__dirname)));
+
+// Example route to serve your main HTML file
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
